595053ec7dc4cac4f20974eef0d3f1d471b4f927
Miguel Ángel Ordóñez <ermigueog@gmail.com>
2020-08-06 13:13:33 +0200

    first commit

---
+++ src/test/java/com/gadium/lostgear/LostGearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gadium.lostgear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostGearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
